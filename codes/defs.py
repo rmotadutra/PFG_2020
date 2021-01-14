4b0d8f893cf4a2405db5665ea7b9af38f4211ed7
Rodrigo Mota <47394022+rmotadutra@users.noreply.github.com>
2021-01-14 08:14:37 -0300

    Versão atualizada

--- codes/defs.py
+++ codes/defs.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def read_litho_types(path):
@@ -403,6 +404,7 @@ def sort_curve (data, statistic, litho_types, curve, step, top=False, base=False
     else:
         base = max(data['DEPTH'])
 
+<<<<<<< Updated upstream
     n_depth = []
     while top <= base:
         n_depth.append(top)
@@ -432,3 +434,186 @@ def sort_curve (data, statistic, litho_types, curve, step, top=False, base=False
         sort_log.append(value)
     
     return np.array(sort_log)
+=======
+
+def corrcoef(steps, curve, depth):
+    passos = steps
+
+    coeficientes_correlação = []
+    intervalo_profundidade = []
+
+    for passo in range(passos): # 'passo' vai de 0 a 300
+
+        # ------------------------------------------ #
+
+        valor_h = [] # h  de head ou cabeça
+        valor_t = [] # t de tail ou cauda
+
+        for i in range(len(curve)-passo): # range menor que o passo para evitar o acesso em indices mais altos e não existentes
+            valor_t.append(curve[i+passo])
+            valor_h.append(curve[i])
+
+        coeficientes_correlação.append(np.corrcoef(valor_h,valor_t)[1][0])
+        intervalo_profundidade.append(depth[0+passo] - depth[0])
+
+    # ------------------------------------------ #
+    
+    return intervalo_profundidade, coeficientes_correlação
+
+def funcao_distribuicao(log,log_info,lito,lito_info, bins,
+                        normalization = False,graph=False,x_scale = False,y_scale = False,
+                        salvar=False,legenda=False,valor_pontual = False):
+    
+    # ................................................ #
+    
+    alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+    
+    self_MY = 0
+    self_lithos_colors = []
+    self_lithos_names  = []
+    self_lithos_index  = []
+    
+    for i in lito_info:
+        self_MY = self_MY + 1
+        self_lithos_colors.append(lito_info[i][0])
+        self_lithos_names.append(lito_info[i][1])
+        self_lithos_index.append(lito[i])
+    
+    self_MX = 0
+    self_max = []
+    self_min = []
+    self_data_names = []
+    self_MEGA_DATA = []
+    self_POINT_VALUES = []
+    
+    for i in log_info:
+        self_MX = self_MX  + 1
+        self_max.append(log_info[i][2][1])
+        self_min.append(log_info[i][2][0])
+        self_data_names.append(log_info[i][0])
+        self_MEGA_DATA.append(log[i])
+        #self_POINT_VALUES.append(valor_pontual[i])
+        
+    #print('MX',self_MX,self_POINT_VALUES) # propriedade
+    #print('MY',self_MY) # litologias
+    
+    # ................................................ #
+
+    def _norm_n(c,p): # incorporate self
+
+        dif = self_max[p] - self_min[p]
+
+        self_normalized_data_a = ((c - self_min[p])/dif)
+
+        return self_normalized_data_a
+
+    # ................................................ #
+
+    def _norm_s(c,p): # incorporate self
+        a = 0.04
+        b = 100
+        self_normalized_data_b = 1.0/(1.0+np.exp(-a*c + a*b) ) 
+
+        return self_normalized_data_b
+
+    # ................................................ #
+
+    def _gaussian(value,mean,sigma):
+
+        gf = (1.0)/(sigma*np.sqrt(2.0*np.pi))
+        gb = (-0.5)*((value - mean)/sigma)**2.0
+        g = gf*np.exp(gb)
+
+        return g
+    
+    # ................................................ #
+
+    self_data_litho_matrix = []
+    gaussian_mean = []
+    gaussian_std = []
+    l = 0
+    
+    # ................................................ #
+    
+    if graph:
+        fig = plt.figure(figsize=(10,10))
+    
+    for k in range(self_MY):
+
+        array_of_curves = []
+
+        m_m = [] # local mean
+        s_s = [] # local gaussian
+        
+        for j in range(self_MX): # 2
+
+            dif = self_max[j] - self_min[j]
+
+            curve = []
+            for i in self_lithos_index[k]:
+                data_point = self_MEGA_DATA[j][i]
+                if normalization:
+                    curve.append(_norm_n(data_point,j))
+                    #VALOR_ISOLADO = _norm_n(self_POINT_VALUES[j],j)
+                else:
+                    curve.append(data_point)
+                    #VALOR_ISOLADO = self_POINT_VALUES[j]
+
+            m = np.mean(curve)
+            s = np.std(curve)
+
+            m_m.append(m)
+            s_s.append(s)
+
+            #P_VALOR_ISOLADO = _gaussian(VALOR_ISOLADO,m,s)
+
+            #print(j,VALOR_ISOLADO,P_VALOR_ISOLADO)
+
+            g_distri = []
+            for i in curve:
+                g_distri.append(_gaussian(i,m,s))
+
+            # ................................................ #
+
+            if graph:
+
+                l = l + 1
+                ax = plt.subplot(len(self_lithos_index),len(self_MEGA_DATA), l)
+                ax.hist(curve,bins,density=True,color=self_lithos_colors[k])
+                
+                ms = '$\overline{m}$ = '+'{:.2f}'.format(m)+'$ | \sigma $ = '+'{:.2f}'.format(s)
+                #mr = 'v ='+'{:.2f}'.format(self_POINT_VALUES[j])+'| n(v) ='+'{:.2f}'.format(VALOR_ISOLADO)+"| P(v) ="+'{:.2f}'.format(P_VALOR_ISOLADO)
+                
+                plt.plot(curve,g_distri,'.b', label='Gaussian'+' '+ms) ### gaussian
+                #plt.plot([VALOR_ISOLADO,VALOR_ISOLADO],[0,P_VALOR_ISOLADO],'-r',linewidth=4,label=mr) ### gaussian
+                plt.legend(loc='upper left',facecolor = 'w')
+
+                if l < len(self_MEGA_DATA) + 1:
+                    plt.title(self_data_names[j],fontsize = 20)
+
+                if l % (len(self_MEGA_DATA))-1 == 0:
+                    plt.ylabel(self_lithos_names[k],fontsize = 20)
+                    
+                ax.annotate('('+alfabeto[l-1]+')', xy=(1, 0), xycoords='axes fraction',
+                                fontsize = 20,horizontalalignment='right', verticalalignment='bottom')
+                
+                if x_scale:
+                    plt.xlim(x_scale)
+                if y_scale:
+                    plt.ylim(y_scale)
+                    
+                ax.patch.set_alpha(0.3)
+
+        gaussian_mean.append(m_m)
+        gaussian_std.append(s_s)
+
+            # ................................................ #
+    
+    if salvar:
+        plt.savefig(salvar[0], transparent=True, dpi = salvar[1], bbox_inches="tight")
+    else:
+        if graph:
+            plt.show()
+        
+    return [gaussian_mean,gaussian_std]
+>>>>>>> Stashed changes
