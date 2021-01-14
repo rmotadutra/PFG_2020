262fcda788d177262fb094764b106aba95d4d629
Rodrigo Mota <47394022+rmotadutra@users.noreply.github.com>
2021-01-14 08:20:25 -0300

    Adicionando grafico gaussiana

--- codes/defs.py
+++ codes/defs.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def read_litho_types(path):
@@ -229,9 +230,9 @@ def separate_lithology(data, litho_types, curves):
     well = data   
     for lith in litho_types:
         well[lith] = {}
-        w = data[curves[0]] == int(litho_types[lith]['CODE'])
+        w = data['LITHOLOGY'] == int(litho_types[lith]['CODE'])
         for curve in curves:
-            well[lith][curve] = np.full(len(data[curves[0]]), np.nan)
+            well[lith][curve] = np.full(len(data['LITHOLOGY']), np.nan)
             well[lith][curve][w] = data[curve][w]
     
     return well
@@ -292,7 +293,7 @@ def formation_zone(data, top, base, curve=False):
     
     return formation
 
-def statistic_lithology(data, lithology, curves, step, top=False, bottom=False, overlap=False):
+def statistic_lithology(data, lithology, curves, step, top = False, base = False):
     """Calculates the statistics for each lithology and for each curve.
 
     Parameters
@@ -359,25 +360,19 @@ def statistic_lithology(data, lithology, curves, step, top=False, bottom=False, 
         top = top
     else:
         top = min(data['DEPTH'])
-
-    if bottom:
-        bottom = bottom
+        
+    if base:
+        base = base
     else:
-        bottom = max(data['DEPTH'])
-
-    if overlap:
-        overlap = overlap
-    else:
-        overlap = 0.0
-
-    tops = []
-    bases = []
-    base = top+step
-    while base <= bottom:
-        tops.append(top)
-        bases.append(base)
-        top = top + step - step*overlap
-        base = base + step - step*overlap
+        base = max(data['DEPTH'])
+    
+    n_depth = []
+    while top <= base:
+        n_depth.append(top)
+        top = top + step
+    
+    tops = n_depth[0:-1]
+    bases = n_depth[1:]
     
     for lith in lithology:
         for curve in curves:
@@ -386,59 +381,60 @@ def statistic_lithology(data, lithology, curves, step, top=False, bottom=False, 
             data[lith][curve]['Data'] = curve_save
             data[lith][curve]['Mean'] = {}
             data[lith][curve]['Std'] = {}
-            mean = []
-            std = []
             for t, b in zip(tops, bases):
                 w = (data['DEPTH'] >= t) & (data['DEPTH'] <= b)
-                m = np.nanmean(curve_save[w])
-                s = np.nanstd(curve_save[w])
-                mean.append(m)
-                std.append(s)
-            data[lith][curve]['Mean']['Top'] = tops
-            data[lith][curve]['Mean']['Base'] = bases
-            data[lith][curve]['Mean']['Value'] = mean
-            data[lith][curve]['Std']['Top'] = tops
-            data[lith][curve]['Std']['Base'] = bases
-            data[lith][curve]['Std']['Value'] = std
+                mean = np.nanmean(curve_save[w])
+                std = np.nanstd(curve_save[w])
+                data[lith][curve]['Mean'][str(t) + '-' + str(b)] = mean
+                data[lith][curve]['Std'][str(t) + '-' + str(b)] = std
                 
     return data
 
-def sort_curve(data, statistic, litho_types, curve, step, top=False, bottom=False):
+def sort_curve (data, statistic, litho_types, curve, step, top=False, base=False):
     
-    import copy
+    import random
     
     if top:
         top = top
     else:
         top = min(data['DEPTH'])
 
-    if bottom:
-        bottom = bottom
+    if base:
+        base = base
     else:
-        bottom = max(data['DEPTH'])
-    
-    gr = np.full(len(data['DEPTH']), np.nan)
-    
-    
-    for (i, depth) in enumerate(data['DEPTH']):
-        for lith in litho_types:
-            tops = copy.deepcopy(statistic[lith][curve]['Mean']['Top'])
-            bases = copy.deepcopy(statistic[lith][curve]['Mean']['Base'])
-            mean = copy.deepcopy(statistic[lith][curve]['Mean']['Value'])
-            std = copy.deepcopy(statistic[lith][curve]['Std']['Value'])
-            if (depth >= top) & (depth <= bottom) & (data['LITHOLOGY'][i] == int(litho_types[lith]['CODE'])):
-                d = list(abs(depth-tops)+abs(depth-bases))
-                index = d.index(min(d))
-                while (np.isnan(mean[index])) and (len(d) > 1):
-                    d.pop(index)
-                    mean.pop(index)
-                    std.pop(index)
-                    index = d.index(min(d))
-                value = np.random.normal(mean[index], std[index])
-                gr[i] = value
-    
-    return np.array(gr)
+        base = max(data['DEPTH'])
 
+<<<<<<< Updated upstream
+    n_depth = []
+    while top <= base:
+        n_depth.append(top)
+        top = top + step
+        
+    sort_log = []
+    for i in range(len(data['LITHOLOGY'])):
+        if np.isnan(data['LITHOLOGY'][i]):
+            value = np.nan
+        else:
+            for k in range(len(n_depth)):
+                for j in range(len(litho_types)):
+                    if int(data['LITHOLOGY'][i]) == int(litho_types[list(litho_types.keys())[j]]['CODE']) and data['DEPTH'][i] >= n_depth[k] and data['DEPTH'][i] <= n_depth[k+1] and data['DEPTH'][i] <= base:
+                        if np.isnan(statistic[list(litho_types.keys())[j]][curve]['Mean'][list(statistic[list(litho_types.keys())[j]][curve]['Mean'])[k]]):
+                            while np.isnan(statistic[list(litho_types.keys())[j]][curve]['Mean'][list(statistic[list(litho_types.keys())[j]][curve]['Mean'])[k]]):
+                                k = k-1
+                            mean = statistic[list(litho_types.keys())[j]][curve]['Mean'][list(statistic[list(litho_types.keys())[j]][curve]['Mean'])[k]]
+                        else:
+                            mean = statistic[list(litho_types.keys())[j]][curve]['Mean'][list(statistic[list(litho_types.keys())[j]][curve]['Mean'])[k]]
+                        if np.isnan(statistic[list(litho_types.keys())[j]][curve]['Std'][list(statistic[list(litho_types.keys())[j]][curve]['Std'])[k]]):
+                            while np.isnan(statistic[list(litho_types.keys())[j]][curve]['Std'][list(statistic[list(litho_types.keys())[j]][curve]['Std'])[k]]):
+                                k = k-1
+                            std = statistic[list(litho_types.keys())[j]][curve]['Std'][list(statistic[list(litho_types.keys())[j]][curve]['Std'])[k]]
+                        else:
+                            std = statistic[list(litho_types.keys())[j]][curve]['Std'][list(statistic[list(litho_types.keys())[j]][curve]['Std'])[k]]
+                        value = random.gauss(mean,std)
+        sort_log.append(value)
+    
+    return np.array(sort_log)
+=======
 
 def corrcoef(steps, curve, depth):
     passos = steps
@@ -463,3 +459,161 @@ def corrcoef(steps, curve, depth):
     # ------------------------------------------ #
     
     return intervalo_profundidade, coeficientes_correlação
+
+def funcao_distribuicao(log,log_info,lito,lito_info, bins,
+                        normalization = False,graph=False,x_scale = False,y_scale = False,
+                        salvar=False,legenda=False,valor_pontual = False):
+    
+    # ................................................ #
+    
+    alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+    
+    self_MY = 0
+    self_lithos_colors = []
+    self_lithos_names  = []
+    self_lithos_index  = []
+    
+    for i in lito_info:
+        self_MY = self_MY + 1
+        self_lithos_colors.append(lito_info[i][0])
+        self_lithos_names.append(lito_info[i][1])
+        self_lithos_index.append(lito[i])
+    
+    self_MX = 0
+    self_max = []
+    self_min = []
+    self_data_names = []
+    self_MEGA_DATA = []
+    self_POINT_VALUES = []
+    
+    for i in log_info:
+        self_MX = self_MX  + 1
+        self_max.append(log_info[i][2][1])
+        self_min.append(log_info[i][2][0])
+        self_data_names.append(log_info[i][0])
+        self_MEGA_DATA.append(log[i])
+        #self_POINT_VALUES.append(valor_pontual[i])
+        
+    #print('MX',self_MX,self_POINT_VALUES) # propriedade
+    #print('MY',self_MY) # litologias
+    
+    # ................................................ #
+
+    def _norm_n(c,p): # incorporate self
+
+        dif = self_max[p] - self_min[p]
+
+        self_normalized_data_a = ((c - self_min[p])/dif)
+
+        return self_normalized_data_a
+
+    # ................................................ #
+
+    def _norm_s(c,p): # incorporate self
+        a = 0.04
+        b = 100
+        self_normalized_data_b = 1.0/(1.0+np.exp(-a*c + a*b) ) 
+
+        return self_normalized_data_b
+
+    # ................................................ #
+
+    def _gaussian(value,mean,sigma):
+
+        gf = (1.0)/(sigma*np.sqrt(2.0*np.pi))
+        gb = (-0.5)*((value - mean)/sigma)**2.0
+        g = gf*np.exp(gb)
+
+        return g
+    
+    # ................................................ #
+
+    self_data_litho_matrix = []
+    gaussian_mean = []
+    gaussian_std = []
+    l = 0
+    
+    # ................................................ #
+    
+    if graph:
+        fig = plt.figure(figsize=(10,10))
+    
+    for k in range(self_MY):
+
+        array_of_curves = []
+
+        m_m = [] # local mean
+        s_s = [] # local gaussian
+        
+        for j in range(self_MX): # 2
+
+            dif = self_max[j] - self_min[j]
+
+            curve = []
+            for i in self_lithos_index[k]:
+                data_point = self_MEGA_DATA[j][i]
+                if normalization:
+                    curve.append(_norm_n(data_point,j))
+                    #VALOR_ISOLADO = _norm_n(self_POINT_VALUES[j],j)
+                else:
+                    curve.append(data_point)
+                    #VALOR_ISOLADO = self_POINT_VALUES[j]
+
+            m = np.mean(curve)
+            s = np.std(curve)
+
+            m_m.append(m)
+            s_s.append(s)
+
+            #P_VALOR_ISOLADO = _gaussian(VALOR_ISOLADO,m,s)
+
+            #print(j,VALOR_ISOLADO,P_VALOR_ISOLADO)
+
+            g_distri = []
+            for i in curve:
+                g_distri.append(_gaussian(i,m,s))
+
+            # ................................................ #
+
+            if graph:
+
+                l = l + 1
+                ax = plt.subplot(len(self_lithos_index),len(self_MEGA_DATA), l)
+                ax.hist(curve,bins,density=True,color=self_lithos_colors[k])
+                
+                ms = '$\overline{m}$ = '+'{:.2f}'.format(m)+'$ | \sigma $ = '+'{:.2f}'.format(s)
+                #mr = 'v ='+'{:.2f}'.format(self_POINT_VALUES[j])+'| n(v) ='+'{:.2f}'.format(VALOR_ISOLADO)+"| P(v) ="+'{:.2f}'.format(P_VALOR_ISOLADO)
+                
+                plt.plot(curve,g_distri,'.b', label='Gaussian'+' '+ms) ### gaussian
+                #plt.plot([VALOR_ISOLADO,VALOR_ISOLADO],[0,P_VALOR_ISOLADO],'-r',linewidth=4,label=mr) ### gaussian
+                plt.legend(loc='upper left',facecolor = 'w')
+
+                if l < len(self_MEGA_DATA) + 1:
+                    plt.title(self_data_names[j],fontsize = 20)
+
+                if l % (len(self_MEGA_DATA))-1 == 0:
+                    plt.ylabel(self_lithos_names[k],fontsize = 20)
+                    
+                ax.annotate('('+alfabeto[l-1]+')', xy=(1, 0), xycoords='axes fraction',
+                                fontsize = 20,horizontalalignment='right', verticalalignment='bottom')
+                
+                if x_scale:
+                    plt.xlim(x_scale)
+                if y_scale:
+                    plt.ylim(y_scale)
+                    
+                ax.patch.set_alpha(0.3)
+
+        gaussian_mean.append(m_m)
+        gaussian_std.append(s_s)
+
+            # ................................................ #
+    
+    if salvar:
+        plt.savefig(salvar[0], transparent=True, dpi = salvar[1], bbox_inches="tight")
+    else:
+        if graph:
+            plt.show()
+        
+    return [gaussian_mean,gaussian_std]
+>>>>>>> Stashed changes
